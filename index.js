d2b6b7cf7250890cd0de19d38387841e279ec957
Glitch (hacker-substep-boi) <none>
2018-05-13 02:00:33 +0000

    🏭💖 Updated with Glitch

--- index.js
+++ index.js
@@ -4,19 +4,15 @@ const client = new Discord.Client();
 const newUsers = new Discord.Collection();
 const config = require("./config.json");
 const fs = require("fs")
-const noPerm = new Discord.RichEmbed()
-    .setTitle("Error!")
-    .setColor("da004e")
-    .setDescription('You do not have permission to use this command!');
 
 //Sets the activity of the bot
 client.on('ready', () => {
-  client.user.setActivity('Hackernight | ' + config.prefix + 'help', {type: 'PLAYING'});
+  client.user.setActivity('Scrubstep | ' + config.prefix + 'help', {type: 'PLAYING'});
 
 //Sends message into the channel with the id that bot has started
   client.channels.get('439819029797142538').send({
         embed: {
-        "color": 14286926,
+        "color": 49663,
         "timestamp": new Date(),
         "footer": {
           "icon_url": client.user.avatarURL,
@@ -31,17 +27,111 @@ client.on('ready', () => {
     });
 });
 
-//Gives/Removes the role with the id when playing/quitting MINDNIGHT
+//Gives/Removes the role with the id when playing/quitting Warframe
 client.on('presenceUpdate', (OldMember, NewMember) => {
-    if (NewMember.presence.game != null && NewMember.presence.game.name == "MINDNIGHT") {
-        NewMember.addRole('439954976018530325').catch(console.error);
+    if (NewMember.presence.game != null && NewMember.presence.game.name == "Warframe") {
+        NewMember.addRole('439947347724730378').catch(console.error);
     }
-    else if (OldMember.presence.game !== null && OldMember.presence.game.name == "MINDNIGHT"
-                && NewMember.presence.game == null || NewMember.presence.game.name != "MINDNIGHT") {
-        if (OldMember.roles.has('439954976018530325')) {
-            OldMember.removeRole('439954976018530325').catch(console.error);
+    else if (OldMember.presence.game !== null && OldMember.presence.game.name == "Warframe"
+                && NewMember.presence.game == null || NewMember.presence.game.name !== "Warframe") {
+        if (OldMember.roles.has('439947347724730378')) {
+            OldMember.removeRole('439947347724730378').catch(console.error);
         }
     }
+});
+
+//Warframe role adder
+client.on("message", (message) => {
+  if (message.author.bot) return; // this blocks the bot from responding to other bots
+  if (message.channel.type === "dm") return; // this prevents dm commands
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+  
+  if (message.content.toLowerCase().startsWith(config.prefix + "addrole warframe") && (!message.member.roles.has('439895350799630346'))) {
+    message.react('✅')
+    message.channel.send({
+      embed: {
+        "color": 49663,
+        "footer": {
+          "icon_url": client.user.avatarURL,
+          "text": client.user.username
+        },
+        "fields": [{
+          "name": "Success!",
+          "value": "Added to Warframe role."
+        }]
+      }
+    })
+     .then(msg => {
+      msg.delete(5000)
+    message.member.addRole('439895350799630346').catch(console.error);
+  })
+  }
+  else if (message.content.toLowerCase().startsWith(config.prefix + "addrole warframe") && (message.member.roles.has('439895350799630346'))) {
+    message.react('❌')
+    message.channel.send({
+      embed: {
+        "color": 16711680,
+        "footer": {
+          "icon_url": client.user.avatarURL,
+          "text": client.user.username
+        },
+        "fields": [{
+          "name": "Error!",
+          "value": "You already have that role. Try " + config.prefix + "removerole warframe"
+        }]
+      }
+    })
+     .then(msg => {
+      msg.delete(5000).catch(console.error);
+        })
+  }
+});
+
+//Warframe role remover
+client.on("message", (message) => {
+  if (message.author.bot) return; // this blocks the bot from responding to other bots
+  if (message.channel.type === "dm") return; // this prevents dm commands
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+  
+  if (message.content.toLowerCase().startsWith(config.prefix + "removerole warframe") && (message.member.roles.has('439895350799630346'))) {
+    message.react('✅')
+    message.channel.send({
+      embed: {
+        "color": 49663,
+        "footer": {
+          "icon_url": client.user.avatarURL,
+          "text": client.user.username
+        },
+        "fields": [{
+          "name": "Success!",
+          "value": "Removed from Warframe role."
+        }]
+      }
+    })
+     .then(msg => {
+      msg.delete(5000)
+    message.member.removeRole('439895350799630346').catch(console.error);
+  })
+  }
+  else if (message.content.toLowerCase().startsWith(config.prefix + "removerole warframe") && (!message.member.roles.has('439895350799630346'))) {
+    message.react('❌')
+    message.channel.send({
+      embed: {
+        "color": 16711680,
+        "footer": {
+          "icon_url": client.user.avatarURL,
+          "text": client.user.username
+        },
+        "fields": [{
+          "name": "Error!",
+          "value": "You don't have that role. Try " + config.prefix + "addrole warframe"
+        }]
+      }
+    })
+     .then(msg => {
+      msg.delete(5000).catch(console.error);
+        })
+  }
 });
 
 //A simple help command
@@ -54,7 +144,7 @@ client.on("message", (message) => {
     message.channel.send({
       embed: {
     "title": "Commands",
-    "color": 14286926,
+    "color": 49663,
     "footer": {
       "icon_url": client.user.avatarURL,
       "text": client.user.username
@@ -86,7 +176,7 @@ client.on("message", (message) => {
   if (message.content.toLowerCase().startsWith(config.prefix + "ping")) {
     message.channel.send({
       embed: {
-        "color": 14286926,
+        "color": 49663,
         "footer": {
           "icon_url": client.user.avatarURL,
           "text": client.user.username
@@ -105,9 +195,21 @@ client.on("message", (message) => {
   if (message.author.bot) return; // this blocks the bot from responding to other bots
   if (message.channel.type === "dm") return; // this prevents dm commands
   if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-  if(message.author.id !== config.ownerID) return message.channel.send({noPerm})
+  if (message.author.id !== config.ownerID) return message.channel.send({
+      embed: {
+        "color": 49663,
+        "footer": {
+          "icon_url": client.user.avatarURL,
+          "text": client.user.username
+        },
+        "fields": [{
+          "name": "Error!",
+          "value": "You do not have permission to use this command!"
+        }]
+      }
+    });
 
-  if(message.content.startsWith(config.prefix + "prefix")) {
+  if (message.content.startsWith(config.prefix + "prefix")) {
   // Gets the prefix from the command (eg. "!prefix +" it will take the "+" from it)
   let newPrefix = message.content.split(" ").slice(1, 2)[0];
   // change the configuration in memory
@@ -117,7 +219,7 @@ client.on("message", (message) => {
   fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
     message.channel.send({
       embed: {
-        "color": 14286926,
+        "color": 49663,
         "footer": {
           "icon_url": client.user.avatarURL,
           "text": client.user.username
